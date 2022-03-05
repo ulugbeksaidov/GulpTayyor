4f4634d88e1ff3f107a8ed648e08e48ca79ab85a
test <test>
2022-03-05 13:03:04 +0500

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+global.$ = {
+    gulp: require('gulp'),
+    gp: require('gulp-load-plugins')(),
+    bs: require('browser-sync').create(),
+    sass: require('gulp-sass')(require('sass')),
+
+    path: {
+        serveDir: './app/build',
+        tasks:require('./gulp/config'),
+
+
+        src:{
+            html:'./app/src/*.html',
+            style: './app/src/style/*.*',
+            js: './app/src/js/*.*',
+            img: './app/src/images/*.{jpg,png,svg,jpeg,jfif,gif,webp}',
+            fonts: '/app/src/fonts/**/*.*',
+        },
+        build:{
+            html:'./app/build',
+            style:'./app/build/style',
+            js: './app/build/js',
+            img: './app/build/images',
+            fonts: './app/build/fonts'
+        },
+        watch:{
+            html:['./app/src/*.html','./app/src/view/*.html'],
+            style:['./app/src/style/*.*','./app/src/style/**/*.*'],
+            js:  './app/src/js/*.*',
+            img: './app/src/images/*.{jpg,png,svg,jpeg,jfif,gif,webp}',
+            fonts: '/app/src/fonts/**/*.*',
+        },
+    },
+    tasks:{
+        default :['html','style','js','img','fonts','serve','watch'],
+        build: ['html' ,'style','js','img','fonts' ]
+    }
+}
+
+$.path.tasks.forEach(taskPath => require(taskPath)());
+
+for (const key in $.tasks) {
+    $.gulp.task(key,$.gulp.series($.gulp.parallel(...$.tasks[key])))
+}
